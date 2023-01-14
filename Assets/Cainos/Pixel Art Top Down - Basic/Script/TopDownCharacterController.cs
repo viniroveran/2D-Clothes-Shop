d503de8d734dd234ff74f5c555ad139b365e539b
Dumbledore <viniroveran@gmail.com>
2023-01-14 12:04:59 -0300

    update character controller for new character

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -19,30 +19,33 @@ namespace Cainos.PixelArtTopDown_Basic
         private void Update()
         {
             Vector2 dir = Vector2.zero;
+            animator.SetBool("isRun", false);
+
             if (Input.GetKey(KeyCode.A))
             {
                 dir.x = -1;
-                animator.SetInteger("Direction", 3);
+                transform.localScale = new Vector3(dir.x * 0.5f, 0.5f, 0.5f);
+                animator.SetBool("isRun", true);
             }
             else if (Input.GetKey(KeyCode.D))
             {
                 dir.x = 1;
-                animator.SetInteger("Direction", 2);
+                transform.localScale = new Vector3(dir.x * 0.5f, 0.5f, 0.5f);
+                animator.SetBool("isRun", true);
             }
 
             if (Input.GetKey(KeyCode.W))
             {
                 dir.y = 1;
-                animator.SetInteger("Direction", 1);
+                animator.SetBool("isRun", true);
             }
             else if (Input.GetKey(KeyCode.S))
             {
                 dir.y = -1;
-                animator.SetInteger("Direction", 0);
+                animator.SetBool("isRun", true);
             }
 
             dir.Normalize();
-            animator.SetBool("IsMoving", dir.magnitude > 0);
 
             GetComponent<Rigidbody2D>().velocity = speed * dir;
         }
