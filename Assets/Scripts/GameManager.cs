a793582cefe47f68d5e3d9309c0d92d9a1b45bf0
Dumbledore <viniroveran@gmail.com>
2023-01-15 19:38:55 -0300

    implement interaction

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,11 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance = null; // Reference to the singleton
     private AsyncOperation _asyncOperation;
     [HideInInspector] public bool isPaused; // Is the game paused?
+    [HideInInspector] public bool isInInteractionRange = false; // Is the player in interaction range?
+    [HideInInspector] public bool canInteractWithSeller = false; // Is the player in interaction range with a seller?
+    [HideInInspector] public bool canInteractWithBuyer = false; // Is the player in interaction range with a buyer?
+    [HideInInspector] public bool isClothesShopOpen = false; // Is the clothes shop UI open?
+    [HideInInspector] public bool isInventoryOpen = false; // Is the inventory UI open?
 
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject clothesShopMenu;
@@ -64,11 +69,15 @@ public class GameManager : MonoBehaviour
             Time.timeScale = 0f;
             
             // Show cursor
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
+            ToggleCursor(true);
             
             // Show Pause Menu UI
             pauseMenu.SetActive(true);
+            
+            // Hide other UIs
+            interactionTooltip.SetActive(false);
+            clothesShopMenu.SetActive(false);
+            inventoryMenu.SetActive(false);
         }
         else
         {
@@ -77,11 +86,50 @@ public class GameManager : MonoBehaviour
             Time.timeScale = 1.0f;
             
             // Hide cursor
-            Cursor.lockState = CursorLockMode.Confined;
-            Cursor.visible = false;
+            ToggleCursor(false);
             
             // Hide Pause Menu UI
             pauseMenu.SetActive(false);
+            
+            // If player was in interaction range, show Interaction Tooltip again
+            if (isInInteractionRange)
+                ToggleInteractionTooltip(true, canInteractWithSeller, canInteractWithBuyer);
+            
+            // If player had the Clothes Shop open before pausing, open it again
+            if (isClothesShopOpen)
+                ToggleClothesShop(true);
+            
+            // If player had the Inventory open before pausing, open it again
+            if (isInventoryOpen)
+                ToggleInventory(true);
         }
+    }
+
+    public void ToggleCursor(bool active)
+    {
+        Cursor.lockState = active ? CursorLockMode.None : CursorLockMode.Confined;
+        Cursor.visible = active;
+    }
+
+    public void ToggleInteractionTooltip(bool active, bool isSeller, bool isBuyer)
+    {
+        isInInteractionRange = active;
+        canInteractWithSeller = isSeller;
+        canInteractWithBuyer = isBuyer;
+        interactionTooltip.SetActive(active);
+    }
+
+    public void ToggleClothesShop(bool active)
+    {
+        ToggleCursor(active);
+        isClothesShopOpen = active;
+        clothesShopMenu.SetActive(active);
+    }
+
+    public void ToggleInventory(bool active)
+    {
+        ToggleCursor(active);
+        isInventoryOpen = active;
+        inventoryMenu.SetActive(active);
     }
 }
--- Assets/Scripts/TopDownCharacterController.cs
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownCharacterController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 3; // character movement speed
     private Rigidbody2D _characterRigidBody2D;
     private GameManager _gameManager;
 
@@ -18,23 +19,85 @@ public class TopDownCharacterController : MonoBehaviour
     {
         Vector2 dir = Vector2.zero;
 
+        // Move Up
         if (Input.GetKey(KeyCode.W))
             dir.y = 1;
         
+        // Move Left
         if (Input.GetKey(KeyCode.A))
             dir.x = -1;
 
+        // Move Down
         if (Input.GetKey(KeyCode.S))
             dir.y = -1;
         
+        // Move Right
         if (Input.GetKey(KeyCode.D))
             dir.x = 1;
 
+        // Normalize dir vector
+        dir.Normalize();
+        // Move character
+        _characterRigidBody2D.velocity = speed * dir;
+        
+        // Interact with NPC
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_gameManager.isInInteractionRange)
+            {
+                // If the NPC is a buyer, open the inventory to sell our items
+                if (_gameManager.canInteractWithBuyer)
+                    _gameManager.ToggleInventory(true);
+
+                // If the NPC is a seller, open the Clothes Shop
+                if (_gameManager.canInteractWithSeller)
+                    _gameManager.ToggleClothesShop(true);
+            }
+        }
+
+        // Open/close Inventory UI
+        if (Input.GetKeyDown(KeyCode.I))
+            _gameManager.ToggleInventory(!_gameManager.isInventoryOpen);
+        
+        // Open/close Pause Menu UI
         if (Input.GetKeyDown(KeyCode.Escape))
             _gameManager.Pause(!_gameManager.isPaused);
 
-        dir.Normalize();
+        // Make sure the cursor is always showing if the Inventory or Clothes Shop UI are open
+        if (_gameManager.isInventoryOpen || _gameManager.isClothesShopOpen)
+        {
+            _gameManager.ToggleCursor(true);
+        }
+    }
 
-        _characterRigidBody2D.velocity = speed * dir;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("BuyerNPC"))
+        {
+            _gameManager.ToggleInteractionTooltip(true, false, true);
+            Debug.Log("Found a Buyer NPC!");
+        }
+
+        if (other.CompareTag("SellerNPC"))
+        {
+            _gameManager.ToggleInteractionTooltip(true, true, false);
+            Debug.Log("Found a Seller NPC!");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("BuyerNPC") || other.CompareTag("SellerNPC"))
+        {
+            _gameManager.ToggleInteractionTooltip(false, false, false);
+            
+            if (_gameManager.isClothesShopOpen)
+                _gameManager.ToggleClothesShop(false);
+            
+            if (_gameManager.isInventoryOpen)
+                _gameManager.ToggleInventory(false);
+            
+            Debug.Log("Moved away from NPC!");
+        }
     }
 }
